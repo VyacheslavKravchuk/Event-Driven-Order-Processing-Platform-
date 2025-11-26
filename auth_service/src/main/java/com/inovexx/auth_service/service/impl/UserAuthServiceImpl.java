0fc2774067e08491b4a8b7c9fed39f6ba1e6f546
VyacheslavKravchuk <viacheslav87klerik@mail.ru>
2025-11-26 11:00:11 +0300

    Добавлен kafka в auth-service (надо будет еще отладить)

--- auth_service/src/main/java/com/inovexx/auth_service/service/impl/UserAuthServiceImpl.java
+++ auth_service/src/main/java/com/inovexx/auth_service/service/impl/UserAuthServiceImpl.java
@@ -1,29 +1,30 @@
 package com.inovexx.auth_service.service.impl;
 
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inovexx.auth_service.dto.UserDto;
 import com.inovexx.auth_service.entity.UserAuth;
 import com.inovexx.auth_service.repository.UserRepository;
 import com.inovexx.auth_service.service.UserAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserAuthServiceImpl implements UserAuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-    private static final String USERNAME_REGEX = "^[a-zA-Z0-9_]{3,20}$";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public UserAuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private static final String USER_TOPIC = "user-events";
 
     @Override
-    public void registerNewUser(UserDto userDto) {
+    public UserAuth registerNewUser(UserDto userDto) throws JsonProcessingException {
 
         if (userRepository.existsByUsername(userDto.getUsername())) {
             throw new IllegalArgumentException("Имя пользователя уже существует.");
@@ -41,6 +42,9 @@ public class UserAuthServiceImpl implements UserAuthService {
         userAuth.setLastName(userDto.getLastName());
         userAuth.setRole(userDto.getRole());
 
-        userRepository.save(userAuth);
+        UserAuth savedUser = userRepository.save(userAuth);
+        String userJson = objectMapper.writeValueAsString(savedUser);
+        kafkaTemplate.send(USER_TOPIC, savedUser.getId().toString(), userJson);
+        return savedUser;
     }
 }
