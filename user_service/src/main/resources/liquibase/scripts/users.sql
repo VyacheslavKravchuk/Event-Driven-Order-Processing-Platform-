eaba260561c0efea12455b6c23f28b501bf60a21
VyacheslavKravchuk <viacheslav87klerik@mail.ru>
2025-11-28 21:57:39 +0300

    Доработаны сервисы

--- user_service/src/main/resources/liquibase/scripts/users.sql
+++ user_service/src/main/resources/liquibase/scripts/users.sql
@@ -1,23 +1,13 @@
--- Создаем тип, ТОЛЬКО ЕСЛИ ОН ЕЩЕ НЕ СУЩЕСТВУЕТ
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'role_enum') THEN
-        CREATE TYPE role_enum AS ENUM ('USER', 'ADMIN', 'MANAGER');
-    END IF;
-END
-$$ LANGUAGE plpgsql;
-
-
 CREATE TABLE IF NOT EXISTS users (
     id BIGINT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    phone_number VARCHAR(50) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    address VARCHAR(250) NOT NULL,
-    role role_enum NOT NULL
+    phone_number VARCHAR(50),
+    city VARCHAR(50),
+    address VARCHAR(250),
+    role VARCHAR(20) NOT NULL
 );
 
 -- Индексы можно создавать без IF NOT EXISTS, ошибки не будет, если они уже есть
